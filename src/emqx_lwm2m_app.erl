bd598ceb7b8d0e3a248ac6c046f157bf8a2c6186
turtled <turtled@emqtt.io>
2019-12-10 11:05:39 +0800

    Rm cfg module

--- src/emqx_lwm2m_app.erl
+++ src/emqx_lwm2m_app.erl
@@ -32,12 +32,10 @@ start(_Type, _Args) ->
     Port = application:get_env(?APP, port, 5683),
     Pid = emqx_lwm2m_sup:start_link(),
     emqx_lwm2m_coap_server:start(Port),
-    emqx_lwm2m_cfg:register(),
     Pid.
 
 prep_stop(State) ->
     emqx_lwm2m_coap_server:stop(),
-    emqx_lwm2m_cfg:unregister(),
     State.
 
 stop(_State) ->
